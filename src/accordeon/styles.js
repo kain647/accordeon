60fc52c7f20f3edb9e5683b88f408ef7186098cf
Aleksey <aleksey.amelchenko@gmail.com>
2020-05-29 17:00:17 +0300

    Basic project setup

--- src/accordeon/styles.js
+++ src/accordeon/styles.js
@@ -1,8 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
-  height: 100vh;
-`
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+  padding: 40px 0;
+`;
+export const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const BlockControl = styled.div`
+  display: flex;
+  width: 500px;
+  background-color: #aaa;
+  padding: 10px;
+  cursor: pointer;
+  margin-bottom: 2px;
+`;
+export const BlockContent = styled.div`
+  width: 500px;
+  padding: 10px;
+  background-color: #eef;
+`;
